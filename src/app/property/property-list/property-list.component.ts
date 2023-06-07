11efd4542f1f20dbd364652f94edb82a4ecc36b0
Necati Akyazi <necati.akyazi@grollmus.de>
2023-06-07 10:24:04 +0200

    tut up to 9

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
-  styleUrls: ['./property-list.component.css']
+  styleUrls: ['./property-list.component.css'],
 })
-export class PropertyListComponent {
+export class PropertyListComponent implements OnInit {
 
+  properties: Array<any> = [
+    {
+      Id: 1,
+      Name: 'Birla House',
+      Type: 'House',
+      Price: 120000,
+    },
+    {
+      Id: 2,
+      Name: 'Birla House 2',
+      Type: 'House',
+      Price: 120000,
+    },
+    {
+      Id: 3,
+      Name: 'Birlas House 3',
+      Type: 'House',
+      Price: 120000,
+    },
+    {
+      Id: 4,
+      Name: 'Birla Appt.',
+      Type: 'Appartement',
+      Price: 12000,
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
